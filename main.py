18ff6a36146efc66d16fcd539185b3c774b25bd6
DanMitozz <117995535+DanMitozz@users.noreply.github.com>
2024-11-21 17:59:35 +0300

    Update main.py
    task5

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ def delete_task(index):
         print(f"Задача '{removed_task['task']}' удалена!")
     else:
         print("Ошибка: Неверный индекс задачи.")  
+
+def mark_completed(index):
+    if 0 <= index < len(tasks):  
+        tasks[index]["completed"] = True  
+        print(f"Задача '{tasks[index]['task']}' выполнена!")
+    else:
+        print("Ошибка: Неверный индекс задачи.") 
